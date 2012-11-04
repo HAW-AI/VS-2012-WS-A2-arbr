94e42c262bf4b3a771dcd21f8f214d5bf3223124
anton <anton@dual-core>
2012-11-04 10:46:09 +0100

    Start ( rebind, setneighbors, setpm)

--- ggt.erl
+++ ggt.erl
@@ -3,7 +3,7 @@
 %% Description: TODO: Add description to ggt
 -module(ggt).
 
--record(ggt_config, {delay,terminatetime,startnummer,starternummer,praktikumsgruppe,teamnummer, koordinatorname,nameservicenode}).
+-record(ggt_config, {arbeitszeit,termzeit,prozessnr,starternr,nameservicepid,praktikumsgruppe, team,koordinatorpid,leftN,rightN,lastSendy}).
 %%
 %% Include files
 %%
@@ -16,36 +16,60 @@
 %%
 %% API Functions
 %%
-start(Delay, Terminatetime,Startnummer, Starternummer,Praktikumsgruppe,Teamnummer, Koordinatorname, Nameservicenode) ->
+start(KoordinatorPID, Arbeitszeit, TermZeit, ProzessNr, StarterNr, NameservicePID, Praktikumsgruppe, Team) ->
 	Config = #ggt_config{
-						 delay = Delay,
-						 terminatetime = Terminatetime,
-						 startnummer = Startnummer,
-						 starternummer = Starternummer,
-						 koordinatorname = Koordinatorname,
+						 arbeitszeit = Arbeitszeit,
+						 termzeit = TermZeit,
+						 prozessnr = ProzessNr,
+						 starternr = StarterNr,
+						 nameservicepid = NameservicePID,
 						 praktikumsgruppe = Praktikumsgruppe,
-						 teamnummer = Teamnummer,
-						 nameservicenode = Nameservicenode
+						 team = Team,
+						 koordinatorpid = KoordinatorPID
 						 },
 	log("Name: ~p",Config,[toString(name(Config))]),
-	spawn(fun() -> log("GGTP ~p (~p) gestartet",Config,[self(), name(Config)]), loop(Config) end).
+	register(name(Config),self()),
+	%Verbindung mit dem Nameserver aufbauen
+	Config#ggt_config.nameservicepid ! {self(), {rebind, name(Config), node()}},
+	receive
+		ok -> log("Verbunden mit ~p",[Config#ggt_config.nameservicepid],Config);
+		kill -> ok %TODO auf kill reagieren
+	after 5000 ->
+			log("Namensdienst ~p antwortet nicht",[Config#ggt_config.nameservicepid],Config)
+	end,
+	% Hello an Koordinator sende
+	Config#ggt_config.koordinatorpid ! {hello, name(Config)},
+	% Infos über die Nachbarn erhalten
+	receive
+		{setneighbors, LeftN, RightN}->
+			log("Ich habe die Nachbarn kennengelernt(L: ~p , R: ~p)",Config,[LeftN,RightN]),
+			NewConfig = Config#ggt_config{leftN = LeftN, rightN=RightN};
+		kill ->
+			ok
+	after 60000 ->
+			log("Ich habe keine Infos bezüglich der Nachbarn erhalten",Config)
+	end,
+	ConfigWithNeighbors = resolve_neighbors(NewConfig), %TODO resolve_neighbors
+	% Mi-Wert vom Koordinator erhalten
+	receive
+		{setpm, MiNeu} -> Mi = MiNeu,
+						  log("Der neu Mi-Wert ist ~p",ConfigWithNeighbors,[MiNeu]),
+						  NewerConfig = ConfigWithNeighbors#ggt_config{lastSendy=now()};
+		kill -> NewerCofig = ConfigWithNeighbors,
+				Mi=0 %TODO kill-routine
+	end,
+	spawn(fun() -> log("GGTP ~p (~p) gestartet",NewerConfig,[self(), name(NewerConfig)]), loop(NewerConfig,Mi) end).
 
 
 %%
 %% Local Functions
 %%
-loop(Config)->
-	%Config#ggt_config.koordinatorname ! {hello, name(Config)},
-	%Config#ggt_config.nameservicenode ! {rebind, name(Config)},
-	register(list_to_atom(toString(name(Config))), self()),
-	receive
-		{setneighbors,LeftN,RightN} -> ok
-	end,
+loop(Config,Mi)->
 	ok.
 
 name(Config) ->
 	%
-	Stringname = toString(Config#ggt_config.praktikumsgruppe)++toString(Config#ggt_config.teamnummer)++toString(Config#ggt_config.startnummer)++toString(Config#ggt_config.starternummer),
+	Stringname = toString(Config#ggt_config.praktikumsgruppe)++toString(Config#ggt_config.team)++toString(Config#ggt_config.prozessnr)++toString(Config#ggt_config.starternr),
 	{Name,_}=string:to_integer(Stringname),
 	Name.
 
